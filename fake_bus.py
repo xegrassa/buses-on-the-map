bd00547cfe181f5c45f29278c91161afc1e870aa
Egrassa <bacek90@mail.ru>
2022-03-13 19:56:36 +0500

    Добавлена отправка нескольких маршрутов движения фейкового транспорта

--- fake_bus.py
+++ fake_bus.py
@@ -1,29 +1,47 @@
 import json
+import os
 from itertools import cycle
 
 import trio
 from trio_websocket import open_websocket_url
 
+SERVER_URL = 'ws://127.0.0.1:8000'
+
+
+def load_routes(directory_path='routes'):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r', encoding='utf8') as file:
+                yield json.load(file)
+
+
+async def run_bus(url, route, bus_id, sem):
+    async with sem:
+        coordinates = cycle(route['coordinates'])
+        try:
+            async with open_websocket_url(url) as ws:
+                for lat, lng in coordinates:
+                    msg = {
+                        "busId": bus_id,
+                        "lat": lat,
+                        "lng": lng,
+                        "route": route['name']
+                    }
+                    await ws.send_message(json.dumps(msg, ensure_ascii=False))
+                    await trio.sleep(2)
+        except OSError as ose:
+            print('Connection attempt failed: %s' % ose)
+
 
 async def main():
-    with open('routes/156.json', 'r', encoding='utf8') as f:
-        route_156 = json.load(f)
-    coordinates = cycle(route_156['coordinates'])
-
-    try:
-        async with open_websocket_url('ws://127.0.0.1:8000') as ws:
-            while True:
-                lat, lng = next(coordinates)
-                msg = {
-                    "busId": "156-0",
-                    "lat": lat,
-                    "lng": lng,
-                    "route": "156"
-                }
-                await ws.send_message(json.dumps(msg))
-                await trio.sleep(1)
-    except OSError as ose:
-        print('Connection attempt failed: %s' % ose)
+    sem = trio.Semaphore(20)
+    async with trio.open_nursery() as nursery:
+        for route in load_routes():
+            async with sem:
+                bus_id = route['name'] + '-qwerty'
+                nursery.start_soon(run_bus, SERVER_URL, route, bus_id, sem)
+                print(f'Запустил {bus_id}')
 
 
 if __name__ == '__main__':
