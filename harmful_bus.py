845f8ececbd5f1bb0c3fb45761643a15ff2b3fb3
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-05-25 17:41:34 +0500

    Add 2 harmful script

---
+++ harmful_bus.py
@@ -0,0 +1,26 @@
+import trio
+import logging
+
+from trio_websocket import open_websocket_url
+
+logger = logging.getLogger(__name__)
+
+
+INVALID_JSON = "a"
+EMPTY_JSON = "{}"
+
+
+async def main():
+    try:
+        async with open_websocket_url("ws://127.0.0.1:8001") as ws:
+            await ws.send_message(INVALID_JSON)
+            await ws.send_message(EMPTY_JSON)
+
+            while True:
+                message = await ws.get_message()
+                print(f"Response: {message}")
+    except OSError as ose:
+        logging.error("Connection attempt failed: %s", ose)
+
+
+trio.run(main)
